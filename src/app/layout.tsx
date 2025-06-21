413be3ed34564618d5dea2d43c674138d9f74cdf
dariomatias-dev <matiasdario75@gmail.com>
2025-06-21 20:09:32 -0300

    feat(src): added `ThemeProvider` component
    - Created and implemented the `ThemeProvider` component.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 
-import { ThemeProvider } from "next-themes";
-
 import "./globals.css";
+import { ThemeProvider } from "@/components/themeProvider";
 
 export const metadata: Metadata = {
   title: "Flutter Guide",
@@ -15,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>
         <ThemeProvider
           attribute="class"
