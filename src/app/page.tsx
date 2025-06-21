c874688cd53c7507cab73e3a9ea64aa78b0c9272
dariomatias-dev <matiasdario75@gmail.com>
2025-06-21 19:58:15 -0300

    refactor(src/app/page.tsx): organized styling and text for consistency
    - Applied organization to styles and texts to improve standardization.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,7 @@ import { Code, Smartphone, Sparkles, Star } from "lucide-react";
 import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { ToggleThemeButton } from "@/components/toggleThemeButton";
 
 export default function Home() {
@@ -17,17 +12,17 @@ export default function Home() {
     "https://play.google.com/store/apps/details?id=com.dariomatias.flutter_guide";
 
   return (
-    <div className="flex min-h-screen w-full flex-col bg-background">
-      <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <div className="bg-background flex min-h-screen w-full flex-col">
+      <header className="bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 w-full border-b backdrop-blur">
         <div className="w-full px-4">
-          <div className="flex h-14 max-w-screen-2xl mx-auto items-center justify-between">
+          <div className="mx-auto flex h-14 max-w-screen-2xl items-center justify-between">
             <Link href="/" className="flex items-center space-x-2">
               <Smartphone className="h-6 w-6" />
-              <span className="font-bold text-lg">Flutter Guide</span>
+              <span className="text-lg font-bold">Flutter Guide</span>
             </Link>
 
             <nav className="flex items-center space-x-4">
-              <Button asChild className="whitespace-nowrap px-4 py-2">
+              <Button asChild className="px-4 py-2 whitespace-nowrap">
                 <Link
                   href={playStoreUrl}
                   target="_blank"
@@ -44,22 +39,23 @@ export default function Home() {
       </header>
 
       <main className="flex-1">
-        <section className="flex justify-center items-center px-4 pb-8 pt-6 md:py-24">
+        <section className="flex items-center justify-center px-4 pt-6 pb-8 md:py-24">
           <div className="flex max-w-screen-md flex-col items-center space-y-6 text-center">
-            <h1 className="text-3xl font-bold leading-tight tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl">
+            <h1 className="text-3xl leading-tight font-bold tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl">
               Your Guide to Mastering Flutter
             </h1>
 
-            <p className="text-lg text-muted-foreground sm:text-xl">
+            <p className="text-muted-foreground text-lg sm:text-xl">
               Learn, practice, and build amazing apps with the best Flutter
               content, right in your pocket.
             </p>
 
-            <p className="text-md text-muted-foreground sm:text-lg max-w-prose">
-              An app developed to showcase the potential and versatility of Flutter.
-              Highlights include responsive interfaces, smooth animations, optimized performance,
-              and seamless API integration. Built to demonstrate how Flutter enables efficient and
-              visually appealing apps for both Android and iOS using a single codebase.
+            <p className="text-md text-muted-foreground max-w-prose sm:text-lg">
+              An app developed to showcase the potential and versatility of
+              Flutter. Highlights include responsive interfaces, smooth
+              animations, optimized performance, and seamless API integration.
+              Built to demonstrate how Flutter enables efficient and visually
+              appealing apps for both Android and iOS using a single codebase.
             </p>
 
             <Button size="lg" asChild>
@@ -76,15 +72,15 @@ export default function Home() {
 
         <section
           id="features"
-          className="flex justify-center items-center bg-slate-50/50 py-8 dark:bg-transparent md:py-12 lg:py-24 px-4"
+          className="flex items-center justify-center bg-slate-50/50 px-4 py-8 md:py-12 lg:py-24 dark:bg-transparent"
         >
-          <div className="flex max-w-screen-lg w-full flex-col items-center space-y-10 text-center">
+          <div className="flex w-full max-w-screen-lg flex-col items-center space-y-10 text-center">
             <div className="space-y-4">
-              <h2 className="text-3xl font-bold leading-[1.1] sm:text-3xl md:text-5xl">
+              <h2 className="text-3xl leading-[1.1] font-bold sm:text-3xl md:text-5xl">
                 Key Features
               </h2>
 
-              <p className="max-w-2xl mx-auto leading-normal text-muted-foreground sm:text-lg sm:leading-7">
+              <p className="text-muted-foreground mx-auto max-w-2xl leading-normal sm:text-lg sm:leading-7">
                 Everything you need to become a skilled Flutter developer.
               </p>
             </div>
@@ -96,12 +92,13 @@ export default function Home() {
                     Widget of the Day
                   </CardTitle>
 
-                  <Sparkles className="h-5 w-5 text-muted-foreground" />
+                  <Sparkles className="text-muted-foreground h-5 w-5" />
                 </CardHeader>
 
                 <CardContent>
-                  <p className="text-sm text-muted-foreground">
-                    Explore a new Flutter widget daily, complete with a practical example.
+                  <p className="text-muted-foreground text-sm">
+                    Explore a new Flutter widget daily, complete with a
+                    practical example.
                   </p>
                 </CardContent>
               </Card>
@@ -112,12 +109,13 @@ export default function Home() {
                     Tips & Examples
                   </CardTitle>
 
-                  <Code className="h-5 w-5 text-muted-foreground" />
+                  <Code className="text-muted-foreground h-5 w-5" />
                 </CardHeader>
 
                 <CardContent>
-                  <p className="text-sm text-muted-foreground">
-                    Get concise coding tips and practical snippets to enhance your Flutter projects.
+                  <p className="text-muted-foreground text-sm">
+                    Get concise coding tips and practical snippets to enhance
+                    your Flutter projects.
                   </p>
                 </CardContent>
               </Card>
@@ -128,12 +126,13 @@ export default function Home() {
                     Practical Guidance
                   </CardTitle>
 
-                  <Star className="h-5 w-5 text-muted-foreground" />
+                  <Star className="text-muted-foreground h-5 w-5" />
                 </CardHeader>
 
                 <CardContent>
-                  <p className="text-sm text-muted-foreground">
-                    Benefit from straightforward guides designed to improve your Flutter development workflow.
+                  <p className="text-muted-foreground text-sm">
+                    Benefit from straightforward guides designed to improve your
+                    Flutter development workflow.
                   </p>
                 </CardContent>
               </Card>
@@ -142,9 +141,9 @@ export default function Home() {
         </section>
       </main>
 
-      <footer className="w-full border-t bg-background px-16">
-        <div className="w-full flex flex-col md:flex-row items-center justify-between gap-4 py-6 text-center md:text-left">
-          <div className="flex flex-col md:flex-row items-center gap-2 text-sm text-muted-foreground">
+      <footer className="bg-background w-full border-t px-16">
+        <div className="flex w-full flex-col items-center justify-between gap-4 py-6 text-center md:flex-row md:text-left">
+          <div className="text-muted-foreground flex flex-col items-center gap-2 text-sm md:flex-row">
             <span>© 2025 Flutter Guide</span>
 
             <span className="hidden md:inline">|</span>
@@ -152,13 +151,13 @@ export default function Home() {
             <span>All rights reserved</span>
           </div>
 
-          <div className="text-sm text-muted-foreground">
+          <div className="text-muted-foreground text-sm">
             Developed by{" "}
             <Link
               href={"https://github.com/dariomatias-dev"}
               target="_blank"
               rel="noopener noreferrer"
-              className="underline hover:text-primary"
+              className="hover:text-primary underline"
             >
               Dário Matias
             </Link>
