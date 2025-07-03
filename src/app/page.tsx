e009d3a45143f513129118b46b15ee8981eb8b44
dariomatias-dev <matiasdario75@gmail.com>
2025-07-03 12:05:58 -0300

    feat(src): componentized FAQ constant
    - Created type definition for FAQ entries.
    - Created dedicated file for the FAQ constant.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,6 +16,7 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 import { Button } from "@/components/ui/button";
+import { faqs } from "@/constants/faqs";
 
 type Testimonial = {
   name: string;
@@ -117,29 +118,6 @@ export default function Home() {
   ];
 
   const testimonials: Testimonial[] = [];
-
-  const faqs = [
-    {
-      question: "Is the app completely free?",
-      answer:
-        "Yes, Flutter Guide is 100% free and open-source. My goal is to provide a high-quality resource for the community at no cost.",
-    },
-    {
-      question: "How often is the content updated?",
-      answer:
-        "New widgets, functions, packages, elements, and UIs are added regularly to keep content up to date.",
-    },
-    {
-      question: "Can I use the code from the examples in my projects?",
-      answer:
-        "Absolutely! All code provided in the app is free to use in your personal and commercial projects. The whole point is to speed up your work.",
-    },
-    {
-      question: "Is the app available for iOS?",
-      answer:
-        "Currently, the app is only available on the Google Play Store for Android. An iOS version is planned for the future, depending on community support and interest.",
-    },
-  ];
 
   const sectionAnimation = {
     initial: { opacity: 0, y: 30 },
---
+++ src/constants/faqs.ts
@@ -0,0 +1,24 @@
+import { FAQ } from "@/@types/FAQ";
+
+export const faqs: FAQ[] = [
+  {
+    question: "Is the app completely free?",
+    answer:
+      "Yes, Flutter Guide is 100% free and open-source. My goal is to provide a high-quality resource for the community at no cost.",
+  },
+  {
+    question: "How often is the content updated?",
+    answer:
+      "New widgets, functions, packages, elements, and UIs are added regularly to keep content up to date.",
+  },
+  {
+    question: "Can I use the code from the examples in my projects?",
+    answer:
+      "Absolutely! All code provided in the app is free to use in your personal and commercial projects. The whole point is to speed up your work.",
+  },
+  {
+    question: "Is the app available for iOS?",
+    answer:
+      "Currently, the app is only available on the Google Play Store for Android. An iOS version is planned for the future, depending on community support and interest.",
+  },
+];
